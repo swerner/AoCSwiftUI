14ea2df13e3ceab4b2d8cee266096643d4a84d80
Scott Werner <stwerner@vt.edu>
2020-11-23 14:35:42 -0500

    Initial Commit

---
+++ AdventOfCode/AdventOfCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdventOfCodeApp.swift
+//  AdventOfCode
+//
+//  Created by Scott Werner on 11/23/20.
+//
+
+import SwiftUI
+
+@main
+struct AdventOfCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
