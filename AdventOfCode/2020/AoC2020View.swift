b629e6d7b33e56938e0dc45e8584b1ef7d7251b5
Scott Werner <scottwerner@hey.com>
2020-12-02 19:21:03 -0500

    Setting up for tomorrow

--- AdventOfCode/2020/AoC2020View.swift
+++ AdventOfCode/2020/AoC2020View.swift
@@ -12,66 +12,213 @@ struct AoC2020View: View {
         VStack {
             NavigationView {
                 VStack {
-                    HStack {
-                        Image(systemName: "star.fill").foregroundColor(.yellow)
-                        Image(systemName: "star.fill").foregroundColor(.yellow)
-                        NavigationLink(destination: Day1View2020()) {
-                            Text("Day 1")
+                    Group {
+                        HStack {
+                            Image(systemName: "star.fill").foregroundColor(.yellow)
+                            Image(systemName: "star.fill").foregroundColor(.yellow)
+                            NavigationLink(destination: Day1View2020()) {
+                                Text("Day 1")
+                            }
                         }
-                    }
-                    
-                    HStack {
-                        Image(systemName: "star.fill").foregroundColor(.yellow)
-                        Image(systemName: "star.fill").foregroundColor(.yellow)
-                        NavigationLink(destination: Day2View2020()) {
-                            Text("Day 2")
+                        
+                        HStack {
+                            Image(systemName: "star.fill").foregroundColor(.yellow)
+                            Image(systemName: "star.fill").foregroundColor(.yellow)
+                            NavigationLink(destination: Day2View2020()) {
+                                Text("Day 2")
+                            }
                         }
-                    }
-                    
-                    HStack {
-                        Image(systemName: "star").foregroundColor(.yellow)
-                        Image(systemName: "star").foregroundColor(.yellow)
-                        NavigationLink(destination: Text("Day 3")) {
-                            Text("Day 3")
+                        
+                        HStack {
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            NavigationLink(destination: Day3View2020()) {
+                                Text("Day 3")
+                            }
+                        }
+                        
+                        HStack {
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            NavigationLink(destination: Text("Day 4")) {
+                                Text("Day 4")
+                            }
+                        }
+                        
+                        HStack {
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            NavigationLink(destination: Text("Day 5")) {
+                                Text("Day 5")
+                            }
+                        }
+                        
+                        HStack {
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            NavigationLink(destination: Text("Day 6")) {
+                                Text("Day 6")
+                            }
+                        }
+                        
+                        HStack {
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            NavigationLink(destination: Text("Day 7")) {
+                                Text("Day 7")
+                            }
+                        }
+                        
+                        HStack {
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            NavigationLink(destination: Text("Day 8")) {
+                                Text("Day 8")
+                            }
+                        }
+                        
+                        HStack {
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            NavigationLink(destination: Text("Day 9")) {
+                                Text("Day 9")
+                            }
+                        }
+                        
+                        HStack {
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            NavigationLink(destination: Text("Day 10")) {
+                                Text("Day 10")
+                            }
                         }
                     }
                     
                     Group {
-                        Text("Day 4")
-                        Text("Day 5")
-                        Text("Day 6")
-                        Text("Day 7")
-                        Text("Day 8")
-                        Text("Day 9")
+                        HStack {
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            NavigationLink(destination: Text("Day 11")) {
+                                Text("Day 11")
+                            }
+                        }
+                        
+                        HStack {
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            NavigationLink(destination: Text("Day 12")) {
+                                Text("Day 12")
+                            }
+                        }
+                        
+                        HStack {
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            NavigationLink(destination: Text("Day 13")) {
+                                Text("Day 13")
+                            }
+                        }
+                        
+                        HStack {
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            NavigationLink(destination: Text("Day 14")) {
+                                Text("Day 14")
+                            }
+                        }
+                        
+                        HStack {
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            NavigationLink(destination: Text("Day 15")) {
+                                Text("Day 15")
+                            }
+                        }
+                        
+                        HStack {
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            NavigationLink(destination: Text("Day 16")) {
+                                Text("Day 16")
+                            }
+                        }
+                        
+                        HStack {
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            NavigationLink(destination: Text("Day 17")) {
+                                Text("Day 17")
+                            }
+                        }
+                        
+                        HStack {
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            NavigationLink(destination: Text("Day 18")) {
+                                Text("Day 18")
+                            }
+                        }
+                        
+                        HStack {
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            NavigationLink(destination: Text("Day 19")) {
+                                Text("Day 19")
+                            }
+                        }
+                        
+                        HStack {
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            NavigationLink(destination: Text("Day 20")) {
+                                Text("Day 20")
+                            }
+                        }
                     }
+                    
                     Group {
-                        Text("Day 10")
-                        Text("Day 11")
-                        Text("Day 12")
-                        Text("Day 13")
-                        Text("Day 14")
-                        Text("Day 15")
-                        Text("Day 16")
-                        Text("Day 17")
-                        Text("Day 18")
-                    }
-                    Group {
-                        Text("Day 19")
-                        Text("Day 20")
-                        Text("Day 21")
-                        Text("Day 22")
-                        Text("Day 23")
-                        Text("Day 24")
-                        Text("Day 25")
+                        HStack {
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            NavigationLink(destination: Text("Day 21")) {
+                                Text("Day 21")
+                            }
+                        }
+                        
+                        HStack {
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            NavigationLink(destination: Text("Day 22")) {
+                                Text("Day 22")
+                            }
+                        }
+                        
+                        HStack {
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            NavigationLink(destination: Text("Day 23")) {
+                                Text("Day 23")
+                            }
+                        }
+                        
+                        HStack {
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            NavigationLink(destination: Text("Day 24")) {
+                                Text("Day 24")
+                            }
+                        }
+                        
+                        HStack {
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            Image(systemName: "star").foregroundColor(.yellow)
+                            NavigationLink(destination: Text("Day 25")) {
+                                Text("Day 25")
+                            }
+                        }
                     }
                 }
             }
         }
-    }
-}
-
-struct AoC2020View_Previews: PreviewProvider {
-    static var previews: some View {
-        AoC2020View()
     }
 }
---
+++ AdventOfCode/2020/Day3/Day3View2020.swift
@@ -0,0 +1,14 @@
+//
+//  Day3View2020.swift
+//  AdventOfCode
+//
+//  Created by Scott Werner on 12/2/20.
+//
+
+import SwiftUI
+
+struct Day3View2020: View {
+    var body: some View {
+        Text("Day 3 view")
+    }
+}
