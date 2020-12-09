922159622c3db6f736290bfa8ee70e6b35a1edd8
Scott Werner <stwerner@vt.edu>
2020-12-08 20:31:38 -0500

    Day 8 - part 2

--- AdventOfCode/2020/Day8/D82020View.swift
+++ AdventOfCode/2020/Day8/D82020View.swift
@@ -9,19 +9,28 @@ import SwiftUI
 
 struct D82020View: View {
     @State private var accumulatorValue: Int = 0
+    @State private var correctAccumulatorValue: Int = 0
     
     var body: some View {
         var instructionProcessor = InstructionProcessor(FileImporter(filename: "d82020-input").lines())
         
         Button(action: {
-            instructionProcessor.runInstructions()
-            accumulatorValue = instructionProcessor.accumulator
+            accumulatorValue = instructionProcessor.accAtLoop()
         }) {
             Text("Run Instructions")
+        }
+        
+        Button(action: {
+            correctAccumulatorValue = instructionProcessor.accWithCorrectInstruction()
+        }) {
+            Text("Find Correct Instruction")
         }
         
         Text("Part 1").bold()
         Text("Number of Instructions: \(instructionProcessor.instructions.count)")
         Text("Accumulator Value before loop: \(accumulatorValue)")
+        
+        Text("Part 2").bold()
+        Text("Accumulator Value With Corrected Instruction: \(correctAccumulatorValue)")
     }
 }
--- AdventOfCode/2020/Day8/InstructionProcessor.swift
+++ AdventOfCode/2020/Day8/InstructionProcessor.swift
@@ -8,14 +8,61 @@
 import Foundation
 
 class InstructionProcessor: NSObject {
-    class Instruction {
-        let operation: String
+    class Instruction: NSObject {
+        var operation: String
         let argument: Int
         
         init(_ instruction: String) {
             let parts = instruction.split(separator: " ")
             operation = String(parts[0])
             argument = Int(parts[1])!
+        }
+    }
+    
+    class InstructionRunner: NSObject {
+        var accumulator: Int = 0
+        var currentInstructionPtr: Int = 0
+        var visitedInstructionPositions: Set<Int> = []
+        var instructions: [Instruction] = []
+        
+        init(_ instructions: [Instruction]) {
+            self.instructions = instructions
+        }
+        
+        func runInstructions() -> Bool {
+            while true {
+                let currentInstruction = instructions[currentInstructionPtr]
+                
+                let insertionResult = visitedInstructionPositions.insert(currentInstructionPtr)
+                
+                if !insertionResult.inserted {
+                    return false
+                }
+                
+                switch currentInstruction.operation {
+                case "jmp":
+                    processJmp(currentInstruction.argument)
+                case "acc":
+                    processAcc(currentInstruction.argument)
+                    currentInstructionPtr += 1
+                case "nop":
+                    currentInstructionPtr += 1
+                default:
+                    currentInstructionPtr += 1
+                }
+                
+                if currentInstructionPtr >= instructions.count {
+                    return true
+                }
+            }
+        }
+        
+        func processJmp(_ amount: Int) {
+            self.currentInstructionPtr += amount
+        }
+        
+        func processAcc(_ amount: Int) {
+            self.accumulator += amount
         }
     }
     
@@ -30,35 +77,47 @@ class InstructionProcessor: NSObject {
         }
     }
     
-    func runInstructions() {
-        while true {
-            let currentInstruction = instructions[currentInstructionPtr]
-            
-            let insertionResult = visitedInstructionPositions.insert(currentInstructionPtr)
-            
-            if !insertionResult.inserted {
-                return
-            }
-            
-            switch currentInstruction.operation {
-            case "jmp":
-                processJmp(currentInstruction.argument)
-            case "acc":
-                processAcc(currentInstruction.argument)
-                currentInstructionPtr += 1
-            case "nop":
-                currentInstructionPtr += 1
-            default:
-                currentInstructionPtr += 1
+    func accAtLoop() -> Int {
+        var instructionRunner = InstructionRunner(self.instructions)
+        instructionRunner.runInstructions()
+        
+        return instructionRunner.accumulator
+    }
+    
+    func flipInstruction(position: Int, instructionList: [Instruction]) -> [Instruction] {
+        let instructionToFlip = instructionList[position]
+        var newInstruction: Instruction
+        
+        if instructionToFlip.operation == "jmp" {
+            newInstruction = Instruction("nop \(instructionToFlip.argument)")
+        } else {
+            newInstruction = Instruction("jmp \(instructionToFlip.argument)")
+        }
+        
+        var newInstructionList = instructionList
+        newInstructionList[position] = newInstruction
+        
+        return newInstructionList
+    }
+    
+    func accWithCorrectInstruction() -> Int {
+        var potentialPositions: [Int] = []
+        
+        for (index, instruction) in instructions.enumerated() {
+            if instruction.operation == "jmp" || instruction.operation == "nop" {
+                potentialPositions.append(index)
             }
         }
-    }
-    
-    func processJmp(_ amount: Int) {
-        self.currentInstructionPtr += amount
-    }
-    
-    func processAcc(_ amount: Int) {
-        self.accumulator += amount
+        
+        for position in potentialPositions {
+            let instructionsToRun = flipInstruction(position: position, instructionList: self.instructions)
+            var instructionRunner = InstructionRunner(instructionsToRun)
+            
+            if (instructionRunner.runInstructions()) {
+                return instructionRunner.accumulator
+            }
+        }
+        
+        return -1;
     }
 }
